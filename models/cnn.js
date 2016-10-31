780098a8e416fc768c6f62bf87d8e5b91cad10f5
Maria Kang <maria96kang@gmail.com>
2016-10-31 12:01:55 -0700

    Fixed package.json

--- models/cnn.js
+++ models/cnn.js
@@ -19,28 +19,13 @@ module.exports = {
 	IMAGE_SIZE: 28,
 	initNetwork: function() {
 		var layerDefs = [];
-		/*
-		layerDefs.push({type:'input', out_sx:24, out_sy:24, out_depth:1}); // declare size of input
-		// output Vol is of size 32x32x3 here
+		
+		layerDefs.push({type:'input', out_sx:24, out_sy:24, out_depth:1});
 		layerDefs.push({type:'conv', sx:5, filters:8, stride:1, pad:2, activation:'relu'});
-		// the layer will perform convolution with 16 kernels, each of size 5x5.
-		// the input will be padded with 2 pixels on all sides to make the output Vol of the same size
-		// output Vol will thus be 32x32x16 at this point
 		layerDefs.push({type:'pool', sx:2, stride:2});
-		// output Vol is of size 16x16x16 here
 		layerDefs.push({type:'conv', sx:5, filters:16, stride:1, pad:2, activation:'relu'});
-		// output Vol is of size 16x16x20 here
 		layerDefs.push({type:'pool', sx:3, stride:3});
-		// output Vol is of size 8x8x20 here
 		layerDefs.push({type:'softmax', num_classes:10});
-		// output Vol is of size 1x1x10 here
-		*/
-		layer_defs.push({type:'input', out_sx:24, out_sy:24, out_depth:1});
-		layer_defs.push({type:'conv', sx:5, filters:8, stride:1, pad:2, activation:'relu'});
-		layer_defs.push({type:'pool', sx:2, stride:2});
-		layer_defs.push({type:'conv', sx:5, filters:16, stride:1, pad:2, activation:'relu'});
-		layer_defs.push({type:'pool', sx:3, stride:3});
-		layer_defs.push({type:'softmax', num_classes:10});
 
 		var net = new convnetjs.Net();
 		net.makeLayers(layerDefs);
